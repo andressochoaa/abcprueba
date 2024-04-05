6231876b569f5f6b39cbd16549140311eff0a81b
Andres Ochoa <andressochoaa@gmail.com>
2024-04-05 02:16:25 -0500

    maquetado del formulario de los campos

--- prueba/rickandmorty/views.py
+++ prueba/rickandmorty/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Personaje
+from .forms import PersonajeForm
 
 def inicio(request):
     return render(request, 'paginas/inicio.html')
@@ -12,7 +13,8 @@ def personajes(request):
     personajes = Personaje.objects.all()
     return render(request, 'personajes/index.html', {'personajes': personajes})
 def crear(request):
-    return render(request, 'personajes/crear.html')
+    formulario = PersonajeForm(request.POST or None)
+    return render(request, 'personajes/crear.html', {'formulario': formulario})
 
 
 def editar(request):
