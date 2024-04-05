3858cc38891e0cab8e1f9739831fb4c7a3d5d90c
Andres Ochoa <andressochoaa@gmail.com>
2024-04-05 01:55:06 -0500

    cambios en el superadmin de django para que muestre la informacion en el nombre y no de forma generica. asi mismo se configuró el borrado de las imagenes cuando se borre un personaje.

--- prueba/rickandmorty/models.py
+++ prueba/rickandmorty/models.py
@@ -6,4 +6,12 @@ class Personaje(models.Model):
     imagen = models.ImageField(upload_to="imagenes/", verbose_name="Imagen", null=True)
     descripcion = models.TextField(verbose_name="Descripción", blank=True)
 
+    def __str__(self):
+        fila = "Nombre: {0}, Imagen: {1}, Descripción: {2}".format(self.nombre, self.imagen, self.descripcion)
+        return fila
+    
+    def delete(self, using=None, keep_parents=False):
+        self.imagen.storage.delete(self.imagen.name)
+        super().delete()
+
 # Create your models here.
