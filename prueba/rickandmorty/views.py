f05ca8432e8a152e05433d7c1ff386d461241d28
Andres Ochoa <andressochoaa@gmail.com>
2024-04-05 02:37:18 -0500

    funcionalidad crear funcionando correctamente

--- prueba/rickandmorty/views.py
+++ prueba/rickandmorty/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Personaje
 from .forms import PersonajeForm
@@ -13,7 +13,10 @@ def personajes(request):
     personajes = Personaje.objects.all()
     return render(request, 'personajes/index.html', {'personajes': personajes})
 def crear(request):
-    formulario = PersonajeForm(request.POST or None)
+    formulario = PersonajeForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('personajes')
     return render(request, 'personajes/crear.html', {'formulario': formulario})
 
 
